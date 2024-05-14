87e596b92c6c142d44a7cbddbcd54324199cd429
si-blinta <nader.ben.ammar.pro@gmail.com>
2024-05-14 18:56:08 +0200

    added test

--- src/host.c
+++ src/host.c
@@ -7,13 +7,14 @@
 
 int main(int argc, char **argv)
 {
-  if (argc < 2)
+  if (argc < 3)
     abort();
   struct dpu_set_t set;
-  uint32_t nb_dpus = 1; 
+  uint32_t nb_dpus = NB_DPU;//atoi(argv[2]); 
   HOST_TOOLS_allocate_dpus(&set,&nb_dpus);
   HOST_TOOLS_compile(1);
   dpu_load(set,DPU_BINARY,NULL);
   srand(time(NULL));
-  HOST_TOOLS_pure_portfolio(argv[1],set);
+  //HOST_TOOLS_pure_portfolio(argv[1],set);
+  HOST_TOOLS_divide_and_conquer(argv[1],set);
 }
